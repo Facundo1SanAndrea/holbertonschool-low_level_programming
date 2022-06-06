9484d938d79b56639934a2d3b3ec09a62a5ee635
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-06 09:22:43 -0700

    Task 101

---
+++ hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *  main - script print 
+ *  Return: one
+ */
+
+int main(void)
+{
+	char str[]= "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+
+	fprintf(stderr, "%s\n", str);
+	return(1);
+}
