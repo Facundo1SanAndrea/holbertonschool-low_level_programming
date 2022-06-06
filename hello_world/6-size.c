c4c1f124114ff4c5c520ce24772973e869b66748
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-06 08:45:18 -0700

    Task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,16 +7,11 @@
 
 int main(void)
 {
-    int intType;
-    float floatType;
-    double doubleType;
-    char charType;
-
-    // sizeof evaluates the size of a variable
-    printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-    printf("Size of float: %zu byte(s)\n", sizeof(floatType));
-    printf("Size of double: %zu byte(s)\n", sizeof(doubleType));
-    printf("Size of a char: %zu byte\n", sizeof(charType));
-    
-    return 0;
+	printf("Size of a char: %zu byte\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+	
+	return 0;
 }
