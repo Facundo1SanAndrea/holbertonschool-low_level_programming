55e82b4ddade7297cd131d3c3c44e8927390907c
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-07 10:01:41 -0700

    Task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - script prints single digit number base ten
+ * Return: zero
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
