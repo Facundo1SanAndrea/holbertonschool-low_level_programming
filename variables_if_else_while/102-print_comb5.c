fc9ce12eccc899eeafd94bc56200278625be905e
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-07 15:54:06 -0700

    Task 102

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+/**
+ * main - prints random number
+ * Return: zero
+ */
+
+int main(void)
+{
+	int i1, i2, i3, i4;
+
+	for (i1 = 0; i1 < 10; i1++)
+	{
+		for (i2 = 0; i2 < 10; i2++)
+		{
+			for (i3 = 0; i3 < 10; i3++)
+			{
+				for (i4 = 0; i4 < 10; i4++)
+				{
+					putchar(i1 + '0');
+					putchar(i2 + '0');
+					putchar(' ');
+					putchar(i3 + '0');
+					putchar(i4 + '0');
+
+					if (i1 == 9 && i2 == 8 && i3 == 9 && i4 == 9)
+						continue;
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
