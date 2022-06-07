62b351f8d0d3d784d2dc5a08f6878b1c6016ecf3
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-07 08:25:33 -0700

    Task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - print alphabet
+ * Return: zero
+ */
+
+int main(void)
+{
+	char alph;
+	char ALPH;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+	 putchar(alph);
+	}
+        for (ALPH = 'A'; ALPH <= 'Z'; ALPH++)
+	{
+	 putchar(ALPH);
+	}
+        putchar('\n');
+
+        return (0);
+
+}	
