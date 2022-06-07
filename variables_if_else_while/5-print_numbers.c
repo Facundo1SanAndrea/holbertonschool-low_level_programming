6b6832bdc3b413a11eea3ef19ac6b10c217173d5
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-07 08:41:53 -0700

    Task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - prints the number zero to nine
+ * Return: zero
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0' ; num <= '9' ; num++)
+	{
+		printf("%d", num)
+	}
+	putchar('\n');
+
+	return (0);
+
+}
