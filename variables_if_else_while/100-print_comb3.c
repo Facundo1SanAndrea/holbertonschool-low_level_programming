62462b279f53b118a4e675ed9e5bf844e9ee2b1b
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-07 15:05:37 -0700

    Task 100

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	int i1, i2;
 	
-	for (i1 = 0; i1 < 10; i1++)
+	for (i1 = 0; i1 < 9; i1++)
 	{
-		for (i2 = 0; i2 < 10; i2++)
+		for (i2 = i1 + 1; i2 < 10; i2++)
 		{
 			putchar((i1 % 10) + '0');
 			putchar((i2 % 10) + '0');
 
 			if ( i1 == 8 && i2 == 9)
-				break;
+				continue;
 			
 			putchar(',');
 			putchar(' ');
