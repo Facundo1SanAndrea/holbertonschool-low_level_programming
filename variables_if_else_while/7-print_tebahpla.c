2b31cbbdb9b7d9cf98f78bcbdff15ce5e2c7d0e0
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-07 09:01:59 -0700

    Task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - prints alphabet revers
+ * Return: zerp
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'z'; alph <= 'a'; alph++)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
