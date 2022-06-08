2e7bba5b4348273aa245475303ebb984df22936a
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-08 11:56:27 -0700

    Task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+# include "main.h"
+
+/**
+ * main - prints alphabet
+ * Return: zero
+ */
+
+void print_alphabet(void)
+{
+	char i;
+	
+	for ( i = 'a'; i <= 'z'; i++)
+	{
+	       	_putchar(i);
+	}
+	_putchar('\n');
+}
