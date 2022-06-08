0739709586522a5db4ff7758e12a363703d601c4
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-08 12:46:34 -0700

    Task 3

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * print_alphabet_10 - prints aplhabet
+ * print_alphabet_x10 - prints aplhabet
  * Return: zero
  */
 
 void print_alphabet_x10(void)
 {
-	 int n = 0;
+	int n = 0;
 
-	 for (; n < 10; n++)
-	 {
-		 char i;
-		 
-		 for ( i = 'a'; i <= 'z'; i++ )
-		 {
-			 _putchar(i);
-		 }
-		 _putchar('\n');
-	 }
+	for (; n < 10; n++)
+	{
+		char i;
+
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
 }
