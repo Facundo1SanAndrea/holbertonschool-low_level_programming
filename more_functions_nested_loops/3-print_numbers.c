337ae6a1dbc0bf97a7e0e3b34c726e9713919308
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-10 14:02:45 -0700

    Task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+void print_numbers(void)
+{
+       int n = 48;
+
+       for (; n < 58; n++)
+       {
+          _putchar(n);
+       }
+       _putchar('\n');
+}
