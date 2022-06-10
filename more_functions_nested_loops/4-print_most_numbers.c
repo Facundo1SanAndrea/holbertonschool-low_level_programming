b1ac7d9b74a3001c4cfa8bc32facee354d97f7fa
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-10 14:15:28 -0700

    Task 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints aplhabet
+ * print_most_numbers - prints aplhabet
  * Return: zero
  */
 
 void print_most_numbers(void)
 {
 	int n = 48;
-	
-	for (;n < 58; n++)
+
+	for (; n < 58; n++)
 	{
 		if (n == 50)
 		{
