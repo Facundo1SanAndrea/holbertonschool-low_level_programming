ce037ba0dcbfc88ea1f12b58d3e1c018170249db
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-12 13:13:40 -0700

    Task 10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,18 +9,31 @@
 
 void print_triangle(int size)
 {
-	int x;
-	int y;
+	int x, y = 0;
+	int space;
 
-	for (x = 1; x <= size; x++)
+	if (size > 0)
 	{
-		for (y = 1; y <= size; y++)
+		while (y < size)
 		{
-			if (y <= size - x)
-				_putchar(' ');
-			else
-				_putchar('#');
+			space = size - y - 1;
+			while (x < size)
+			{
+				if (space > x)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+				x++;
+			}
+			y++;
+			x = 0;
+			_putchar ('\n');
 		}
-		_putchar('\n');
 	}
+	else
+		_putchar('\n');
 }
