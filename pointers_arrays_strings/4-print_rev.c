04690630880ef491f0d589c3ae261d346accf71b
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-14 11:08:35 -0700

    Task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,13 @@ void print_rev(char *s)
 	int a, b;
 
 	a = 0;
-	while(s[a] != '\0')
+	while (s[a] != '\0')
 	{
 		a++;
 	}
-	
+
 	b = a - 1;
-	while(s[b] != 0)
+	while (s[b] != 0)
 	{
 		_putchar(s[b]);
 		b--;
