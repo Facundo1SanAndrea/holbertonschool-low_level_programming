8db5cf9c35bf33199b9f4d2f25c3e37062bcd163
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-14 09:25:50 -0700

    Task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  swap_int - swap numbers
+ *@a - number define
+ *@b - define number
+ *Return: zero
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swap;
+
+	swap = *a;
+
+	*a = *b;
+
+	*b = swap;
+}
