05329865bf838aea8d592472bb1c448ba2a8f5e1
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-16 11:28:32 -0700

    Task 1.3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *_strcmp - value
+ *_strcmp - count differ one to other
  *@s1: value
  *@s2: value
- *Return : zero
+ *Return: diff
  */
 
 int _strcmp(char *s1, char *s2)
@@ -15,7 +15,7 @@ int _strcmp(char *s1, char *s2)
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		diff = (s1[i] - s2[i]);
-	
+
 		if (diff != 0)
 			break;
 	}
