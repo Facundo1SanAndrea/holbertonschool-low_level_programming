3e25da7a8a7168b130e156a17c86d68cd47dffcb
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-17 06:31:14 -0700

    Task 1.6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *cap_string - change lowercase into upper
+ *@str: value
+ *Return: value
+ */
+
+char *cap_string(char *s)
+{
+	char sib[] = { 32 , 9 , 10 , 44 , 59 , 46 , 33 , 63 , 34 , 40 , 41 , 123 , 125 };
+	int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (i == 0)
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+			}
+		}
+		for (j = 0; sib[j]; j++)
+		{
+			if (s[i - 1] == sib[j])
+			{
+				if (s[i] >= 'a' && s[i] <= 'z')
+				{
+					s[i] = s[i] - 32;
+				}
+			}
+		}
+	}
+	return (s);
+}
