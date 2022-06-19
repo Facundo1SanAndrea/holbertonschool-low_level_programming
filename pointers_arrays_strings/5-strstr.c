6a57aa92e8a0f71df04ac5c6c2d4b75dd0887340
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-19 10:52:47 -0700

    Task 3.5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -20,18 +20,15 @@ char *_strstr(char *haystack, char *needle)
 
 		if (haystack[i] == needle[i])
 		{
-			do {
-				if (needle[i + 1] == '\0')
-					return (haystack);
-
-				i++;
+			if (needle[i + 1] == '\0')
+				return (haystack);
+			i++;
 
 			}
-			while (haystack[i] == needle[i]);
-		}
-
-		haystack++;
+		
+		while (haystack[i] == needle[i]);
 	}
+	haystack++;
 
 	return ('\0');
 }
