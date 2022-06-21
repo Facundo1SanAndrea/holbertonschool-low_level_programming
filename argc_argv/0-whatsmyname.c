a51345a060185cd162791e0f85c1d62b4481c96e
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-21 08:17:23 -0700

    Task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,15 +2,16 @@
 #include "main.h"
 
 /**
- *main - change name
+ *main - change name of a file
  *@argc: value
  *@argv: value
+ *Return: value
  */
 
 int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
