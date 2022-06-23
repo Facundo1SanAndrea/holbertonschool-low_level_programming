f91223b845950271605415385c2da0078c130357
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-23 09:42:53 -0700

    Task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include<stdlib.h>
+/**
+ *_strlen - prints number string
+ * @s: vlue checked
+ * Return:sero
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
+}
+
+/**
+ *_strdup - return copy str
+ *@str: value
+ *Return: value
+ */
+
+char *_strdup(char *str)
+{
+	char *result = malloc(_strlen(str) + 1);
+	char *prsc = str;
+	char *pdst = result;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (*prsc != 0)
+	{
+		*pdst = *prsc;
+		pdst++;
+		prsc++;
+	}
+	*pdst = '\0';
+	return (result);
+}
