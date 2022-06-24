fbee419f603eac4299572812ed741be291526965
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-06-24 10:02:25 -0700

    Task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -41,10 +41,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		{
 			result[i] = s1[i];
 		}
-		result[i + j] = '\0';
 		if (n >= n2)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j < n2; j++)
 			{
 				result[i + j] = s2[j];
 			}
@@ -52,7 +51,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		}
 		if (n < n2)
 		{	
-			for (j = 0; j < n2; j++)
+			for (j = 0; j < n; j++)
 			{
 				result[i + j] = s2[j];
 			}
