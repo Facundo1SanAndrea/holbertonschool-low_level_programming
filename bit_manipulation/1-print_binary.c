3d142d835673a375675b1fae55725d093a100bb8
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-07-19 10:24:00 -0700

    Task 1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *print_binary - prints binary
+ *@n: value
+ *Return: value
+ */
+
+void print_binary(unsigned long int n)
+{
+unsigned long int res = 0;
+int i, flag = 0;
+
+	if (n == 0)
+	{
+		_putchar ('0');
+		return;
+	}
+	for (i = 63; i >= 0; i--)
+	{
+		res = n >> i;
+
+		if (res & 1)
+		{
+			_putchar ('1');
+			flag++;
+		}
+		else if (flag)
+		{
+			_putchar('0');
+		}
+	}
+}
