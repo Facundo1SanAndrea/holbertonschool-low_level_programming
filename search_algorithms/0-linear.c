52966facd74ce276bdce66186572b82f5289d22a
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-08-24 06:59:09 -0700

    Adding algorithms of search on task 0

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,18 @@
+#include "search_algos.h"
+/**
+ *linear_search - Given an array arr[] of N elements, the task is to write a function to search a given element x in arr[].
+ *@array: array of elements
+ *@size: size
+ *@value: value
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
