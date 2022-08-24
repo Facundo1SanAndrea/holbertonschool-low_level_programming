3dda4b35beb602d8d13c63f888678979d98ddf82
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-08-24 07:13:33 -0700

    Fixing betty

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,6 +1,7 @@
 #include "search_algos.h"
 /**
- *linear_search - Given an array arr[] of N elements, the task is to write a function to search a given element x in arr[].
+ *linear_search - Given an array arr[] of N elements, i
+ the task is to write a function to search a given element x in arr[].
  *@array: array of elements
  *@size: size
  *@value: value
@@ -12,7 +13,10 @@ int linear_search(int *array, size_t size, int value)
 	for (i = 0; i < size; i++)
 	{
 		if (array[i] == value)
+		{
+			print_array(array, size);
 			return (i);
+		}
 	}
 	return (-1);
 }
